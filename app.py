6c74d4e4ec2b4e84a446fa95fca85ffffc5e6614
sapphirethunderbird <whisnantscarlett817@gmail.com>
2025-05-25 15:20:34 +0900

    add everything else

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Never gonna give u up, never gonna let u down"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
